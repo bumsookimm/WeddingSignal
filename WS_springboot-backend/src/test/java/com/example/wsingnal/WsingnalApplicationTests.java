af0ba4a520ef1a822800f3f1842ffa2bf67048e5
BUMOK\rhaen <rhaenfdlekd2@naver.com>
2025-02-27 17:37:27 +0900

    react, springboot 초기세팅 진행

---
+++ WS_springboot-backend/src/test/java/com/example/wsingnal/WsingnalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsingnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsingnalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
