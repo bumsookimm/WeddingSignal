1d614a923e45cd7132d4c9a152820151065fb950
BUMOK\rhaen <rhaenfdlekd2@naver.com>
2025-02-28 00:41:54 +0900

    프론트엔드 - header 레이아웃, main 레이아웃, 로그인 레이아웃 진행 중

--- WS_react-frontend/WSingnal_frontend/src/App.js
+++ WS_react-frontend/WSingnal_frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';  // Header 컴포넌트 임포트
+import MainPage from './components/MainPage';   // MainPage 컴포넌트 임포트
+import LoginPage from './components/LoginPage'; // LoginPage 컴포넌트 임포트
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />  {/* 상단 바를 모든 페이지에 표시 */}
+      <Routes>
+        <Route path="/" element={<MainPage />} />  {/* 메인 페이지 */}
+        <Route path="/login" element={<LoginPage />} />  {/* 로그인 페이지 */}
+
+      </Routes>
+    </Router>
   );
 }
 
